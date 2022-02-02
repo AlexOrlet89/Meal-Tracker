6ca0fb7e3ee60dd2c0c98736d7930403f87ffada
Alex Orlet <AlexOrlet89@gmail.com>
2022-02-01 17:31:34 -0800

    event listener for form started, pushes to array and valid entries

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
-// import functions and grab DOM elements
+const form = document.getElementById('add-ingredients');
+const ingredientslist = document.getElementById('activeIngredients');
 
-// let state
+let ingredients = [];
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const formData = new FormData(form);
+    const ingredient = {
+        ingredient: formData.get('ingredient'),
+        quantity: formData.get('quantity'),
+        measurement: formData.get('measurement')
+    }; 
+    ingredients.push(ingredient);
+    form.reset();
+    console.log(ingredients);
+});
